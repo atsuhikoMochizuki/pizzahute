f6d38cbb5e23cb7e895aa94f6c8c3e3849a77061
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-04 13:58:33 +0200

    Ajout de la fonction launchClientBrowser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+const child_process = require("child_process");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "PIZZAHUTTE - Pizzéria collaborative située à Perpet-les-Pranades",
+  });
 });
 
 module.exports = router;
