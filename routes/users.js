c52defa471cc6c8909e574022d8ea384b0ed86a3
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-05 01:28:40 +0200

    Menu de navigation fonctionnel

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.render("users", {
+    title: "PIZZAHUTTE - Pizzéria à Perpette-les-pranades(Doubs)",
+  });
 });
-
 module.exports = router;
